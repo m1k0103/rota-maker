756ad7463780de54433b588924d3f9aea4e1e23d
mikok <84585119+miko1034@users.noreply.github.com>
2024-09-23 21:02:36 +0100

    idk what changed ill be honest

---
+++ chippy_rota/__init__.py
@@ -0,0 +1,53 @@
+import sqlite3
+import os
+import yaml
+
+def get_db_name():
+    with open("config.yaml") as f:
+        conf = yaml.load(f, Loader=yaml.FullLoader)
+        DB_NAME = conf["database_name"]
+        return DB_NAME
+    
+def get_admin_details():
+    with open("config.yaml") as f:
+        conf = yaml.load(f, Loader=yaml.FullLoader)
+        ADMIN_USERNAME = conf["admin_username"]
+        ADMIN_PASSWORD = conf["admin_password"]
+        ADMIN_SURNAME = conf["admin_surname"]
+        return ADMIN_USERNAME, ADMIN_SURNAME, ADMIN_PASSWORD
+
+
+def start():
+    DB_NAME = get_db_name()
+
+    if DB_NAME not in os.listdir():
+        db = open(f"./{DB_NAME}", "w+")
+        db.close()
+
+        con = sqlite3.connect(DB_NAME)
+        cursor = con.cursor()
+    
+        cursor.execute("""CREATE TABLE employees(
+                       eid INTEGER PRIMARY KEY,
+                       name TEXT,
+                       surname TEXT
+                       )""")
+        cursor.execute("""CREATE TABLE availability(
+                       employee_id INT,
+                       day TEXT,
+                       start_time TEXT,
+                       end_time TEXT,
+                       FOREIGN KEY (employee_id) REFERENCES employees(eid)
+                       )""")
+        con.commit()
+
+        # creates default admin details
+        ADMIN_USER,ADMIN_SURNAME,ADMIN_PASSWORD = get_admin_details()
+        cursor.execute("INSERT INTO employees(name,surname) VALUES (?,?)", [ADMIN_USER,ADMIN_SURNAME])
+
+        con.commit()
+        con.close()
+        print("db created")
+
+    from chippy_rota.routes import app
+    app.run(host="0.0.0.0", port="80",debug=True)

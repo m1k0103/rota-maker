051fd7db4e9f1d6bfc5937a5f5cba68145ca5291
mikok <84585119+miko1034@users.noreply.github.com>
2024-09-24 19:26:58 +0100

    add availability route and update availability

--- chippy_rota/routes.py
+++ chippy_rota/routes.py
@@ -15,22 +15,22 @@ def index():
         all_employees = DB.get_all_employees()        
         return render_template("index.html", employee_list=all_employees)
 
-@app.route("/get_employee_details", methods=["POST"])
-def get_employee_details():
+@app.route("/get_employee_shifts", methods=["POST"])
+def get_employee_shifts():
     rjson = request.json
     employee_name = rjson["employee_name"]
     shifts = DB.get_shift_info(employee_name)
     print(shifts)
-    return render_template("employee_details.html",name=employee_name,shifts=shifts)
+    return render_template("employee_shifts.html",name=employee_name,shifts=shifts)
 
-@app.route("/update_rota",methods=["POST"])
-def update_rota():
+@app.route("/update_shifts",methods=["POST"])
+def update_shifts():
     shift_id = request.form["shift_id"]
     from_time = request.form["from_time"]
     to_time = request.form["to_time"]
     date = request.form["date"]
     name = request.form["employee_name"]
-    DB.update_rota(id=shift_id,name=name,to_time=to_time,from_time=from_time,day=date)
+    DB.update_shifts(id=shift_id,name=name,to_time=to_time,from_time=from_time,day=date)
     return redirect(url_for("index"))
 
 @app.route("/create_shift", methods=["POST"])
@@ -38,6 +38,17 @@ def create_shift():
     name = request.form["name"]
     DB.create_blank_shift(name)
     return redirect(url_for("index"))
+
+@app.route("/add_availability", methods=["POST"])
+def add_availability():
+    rjson = request.json
+    employee_name = rjson["employee_name"]
+    return render_template("employee_availability.html", name=employee_name)
+
+@app.route("/update_availability", methods=["POST"])
+def update_availability():
+    days = [request.form["mon"],request.form["tue"],request.form["wed"],request.form["thu"],request.form["fri"],request.form["sat"]]
+    hours = [request.form["monRange"],request.form["tueRange"],request.form["wedRange"],request.form["thuRange"],request.form["friRange"],request.form["satRange"]]
 
 def generate_rota_message():
     pass

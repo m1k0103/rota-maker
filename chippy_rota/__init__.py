9738299b19be915654000b0a294a83c76c035596
mikok <84585119+miko1034@users.noreply.github.com>
2024-09-29 18:48:21 +0100

    changes to db, __init__ changes

--- chippy_rota/__init__.py
+++ chippy_rota/__init__.py
@@ -1,21 +1,7 @@
 import sqlite3
 import os
 import yaml
-
-def get_db_name():
-    with open("config.yaml") as f:
-        conf = yaml.load(f, Loader=yaml.FullLoader)
-        DB_NAME = conf["database_name"]
-        return DB_NAME
-    
-def get_admin_details():
-    with open("config.yaml", "r") as f:
-        conf = yaml.load(f, Loader=yaml.FullLoader)
-        ADMIN_USERNAME = conf["admin_username"]
-        ADMIN_PASSWORD = conf["admin_password"]
-        ADMIN_SURNAME = conf["admin_surname"]
-        return ADMIN_USERNAME, ADMIN_SURNAME, ADMIN_PASSWORD
-
+from chippy_rota.func import Database,get_db_name,get_admin_details
 
 def start():
     #creates the config.yaml file
@@ -41,8 +27,7 @@ def start():
         cursor.execute("""CREATE TABLE employees(
                        eid INTEGER PRIMARY KEY,
                        name TEXT,
-                       surname TEXT,
-                       max_shifts INT
+                       surname TEXT
                        )""")
         cursor.execute("""CREATE TABLE shifts(
                        employee_id INT,
@@ -61,17 +46,18 @@ def start():
                        fri TEXT,
                        sat TEXT,
                        sun TEXT,
+                       max_shifts INT,
                        FOREIGN KEY (employee_id) REFERENCES employees(eid)
                        )""")
         con.commit()
-
-        # creates default admin details
-        ADMIN_USER,ADMIN_SURNAME,ADMIN_PASSWORD = get_admin_details()
-        cursor.execute("INSERT INTO employees(name,surname,max_shifts) VALUES (?,?,0)", [ADMIN_USER,ADMIN_SURNAME])
-
-        con.commit()
         con.close()
         print("db created")
+
+        # creates default admin details
+        DB = Database(get_db_name())
+        ADMIN_USER,ADMIN_SURNAME,ADMIN_PASSWORD = get_admin_details()
+        #DB.add_employee(ADMIN_USER,ADMIN_SURNAME)
+
 
     from chippy_rota.routes import app
     app.run(host="0.0.0.0", port="5000",debug=True)
